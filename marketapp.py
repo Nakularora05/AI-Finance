78e08cbe3e295d304749a2b260e6fffe20befeb7
Nakularora05 <141902050+Nakularora05@users.noreply.github.com>
2025-03-24 14:50:58 +0530

    marketapp.py

--- marketapp.py
+++ marketapp.py
@@ -49,7 +49,7 @@ def page1():
     st.markdown('<p class="title-text">📊 Stock Insight AI - Technical Analysis</p>', unsafe_allow_html=True)
 
     # Display candlestick chart image
-    st.image("https://www.investopedia.com/thmb/WF8qDH7lnqEp4NZdzICRGt-FKsk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/dotdash-investopedia-images-technical-analysis-candlestick-5801cfd3c91b4e28b74ff180a574c7db.png", caption="Candlestick Chart Example", use_column_width=True)
+    st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Candlestick_chart_scheme_03-en.svg/640px-Candlestick_chart_scheme_03-en.svg.png", caption="Candlestick Chart Example", use_column_width=True)
 
     col1, col2 = st.columns(2)
     with col1:
